cbbe2b04cc90accd8c21105837bf563f077a73a6
StefanHaslhofer <haslhofer.stefan@gmail.com>
2021-10-13 21:04:58 +0200

    assignment 1 solution v1

--- ai_assigments_1/ai_assignments/search/bfs.py
+++ ai_assigments_1/ai_assignments/search/bfs.py
@@ -16,4 +16,22 @@ class BFS(object):
     # - use the 'queue' datastructure that is already imported as the 'fringe'/ the 'frontier'
     # - use 'problem.successors(node)' to get a list of nodes containing successor states
     def solve(self, problem: Problem):
-        return None
+        visited = set()
+        queue = Queue()
+        queue.put(problem.get_start_node())
+
+        current = queue.get()
+
+        while not problem.is_end(current):
+
+            successors = problem.successors(current)
+
+            if current not in visited:
+                visited.add(current)  # mark current element as visited
+                for s in successors:
+                    queue.put(s)    # put successor into queue if it has not been visited
+
+            if queue.has_elements():  # check if queue has elements left
+                current = queue.get()
+
+        return current
