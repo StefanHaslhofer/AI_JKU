3757d77dfa03b18856d8d42b81310edc86e944f4
StefanHaslhofer <haslhofer.stefan@gmail.com>
2022-01-05 22:22:45 +0100

    assignment 5 a)

--- ai_assignment_5/ai_assignments/bayesian_nets/construct_a_net.py
+++ ai_assignment_5/ai_assignments/bayesian_nets/construct_a_net.py
@@ -1,8 +1,14 @@
 import hedgehog as hh
 
+# adventure is seperate because it doesn´t influence the study decision
 bn = hh.BayesNet(
-    ('Burglary', 'Alarm'),
-    ('Earthquake', 'Alarm'),
-    ('Alarm', 'John calls'),
-    ('Alarm', 'Mary calls')
+    ('Sunny', 'Party'),
+    ('Weekend', 'Party'),
+    (['Weekend', 'Party', 'Likes subject'], 'Study'),
+    ('Adventure')
 )
+
+dot = bn.graphviz()
+dot.attr(label=r'\n\n Bayesian Network \n drawn by Stefan Haslhofer (K11908757 )')
+
+path = dot.render('burglary_title', directory='q1', format='png', cleanup=True)
